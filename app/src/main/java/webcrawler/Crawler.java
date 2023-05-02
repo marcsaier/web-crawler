c791c7eb50dc8a4d9094b3a1cffde81bdee566b6
Marc <marc98711@gmail.com>
2023-05-02 14:53:00 +0200

    initial commit

---
+++ app/src/main/java/webcrawler/Crawler.java
@@ -0,0 +1,27 @@
+package webcrawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+
+public class Crawler {
+
+    public static void main(String[] args) {
+        String url = args[0];
+
+        try {
+            Document doc = Jsoup.connect(url).get();
+            
+            String webSite = url;
+            Pattern email = Pattern.compile("[a-zA-Z\\.]{2,20}@[a-zA-Z\\.-]{2,20}\\.[az]{2}");
+            Matcher matcher = email.matcher(webSite.toString());
+            while (matcher.find()) {
+                System.out.println(matcher.group());//Print email addresses to screen
+            }
+        }
+        catch (IOException e) {
+            System.err.println("A Error occured!");
+            e.printStackTrace();
+        }
+    }
+}
