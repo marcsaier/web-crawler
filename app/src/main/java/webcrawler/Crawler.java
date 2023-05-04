182a986a5207aba243252ae18a4dde863d32aa78
Marc Saier <marc98711@gmail.com>
2023-05-04 10:33:26 +0200

    v0.b - work in progress...

--- app/src/main/java/webcrawler/Crawler.java
+++ app/src/main/java/webcrawler/Crawler.java
@@ -1,10 +1,7 @@
 package webcrawler;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+
 
 public class Crawler {
 
@@ -12,16 +9,13 @@ public class Crawler {
         String url = args[0];
 
         try {
-            Document webSite = Jsoup.connect(url).get();
-            Pattern email = Pattern.compile("[a-zA-Z\\.]{2,20}@[a-zA-Z\\.-]{2,20}\\.[com|org|net|edu|de]{3}");
-            Matcher matcher = email.matcher(webSite.toString());
-            while (matcher.find()) {
-                System.out.println(matcher.group());//Print email addresses to screen
-            }
+            Scraper scraper = new Scraper(url);
+            scraper.start();
+            
         }
-        catch (IOException e) {
-            System.err.println("A Error occured!");
-            e.printStackTrace();
+        catch (Exception e) {
+            System.err.println("Something went wrong!");
         }
+        
     }
 }
---
+++ app/src/main/java/webcrawler/Scraper.java
@@ -0,0 +1,52 @@
+package webcrawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Scraper extends Thread {
+    String url;
+    ArrayList<String> followUrls = new ArrayList<>();
+
+    public Scraper (String url) {
+        this.url = url;
+    }
+
+    @Override
+    public void run () {
+
+        try {
+            Document webSite = Jsoup.connect(url).get();
+            Pattern email = Pattern.compile("[a-zA-Z\\.]{2,20}@[a-zA-Z\\.-]{2,20}\\.[com|org|net|edu|de]{3}");
+            Pattern link = Pattern.compile("href=\"([a-zA-Z\\./-:0-9]{5,25})\"");
+            Matcher matcher = email.matcher(webSite.toString());
+            Matcher matcher2 = link.matcher(webSite.toString());
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+            while (matcher2.find()) {
+                String match = matcher.group(1);
+                followUrls.add(match);
+            }
+            System.out.println("Bis hier ist alles gut");
+            for (int i=0; i<=followUrls.size(); i++){
+                System.out.println(followUrls.get(i));
+            }    
+            
+            
+        }
+        catch (IllegalStateException e) {
+            System.err.println("Keine URLS gefunden auf dieser Seite");
+        }
+        catch (IOException e) {
+            System.err.println("A Error occured!");
+            e.printStackTrace();
+        }
+    }
+    public ArrayList<String> getFollowUrls() {
+        return followUrls;
+    }
+}
