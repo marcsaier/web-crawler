064b3247c45c50b056585365ab04a12a61887e4d
Marc Saier <marc98711@gmail.com>
2023-05-09 15:31:24 +0200

    v0.3c

---
+++ app/src/main/java/webcrawler/Main.java
@@ -0,0 +1,15 @@
+package webcrawler;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
+        primaryStage.show();
+    }
+
+}
